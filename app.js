3e7e50ab1cb57f773933266879469084e5c85cc8
ANS <36783010+answebdev@users.noreply.github.com>
2023-10-08 18:26:30 -0700

    display fetched data

--- app.js
+++ app.js
@@ -18,16 +18,23 @@ fetch('https://the-prisoner-api.vercel.app/episodes')
                     <h3 id='title'>${item.title}</h3>
                     <div class='details'>
                         <p>
-                            <strong>Season: </strong>${item.season}<br />
                             <strong>Episode: </strong>${item.episode}<br />
-                            <strong>Year: </strong>${item.air_year}
+                            <strong>Year: </strong>${item.air_year}<br />
+                            <strong>Date: </strong>${item.air_date}
                         </p>
                     </div>
-                    <img id='image' src=${item.img} alt='${item.title}' title='${item.title}' />
+                    <img id='image' src=${item.title_img} alt='${item.title}' title='${item.title}' />
                     <br />
-                    <p class='narration'><strong>Opening Narration: </strong>${item.opening_narration}</p>
-                    <p class='narration'><strong>Closing Narration: </strong>${item.closing_narration}</p>
+                    <a href=${item.wikipedia} rel='noopener noreferrer' target='_blank'>Wikipedia</a> 
+                    <a href=${item.imdb} rel='noopener noreferrer' target='_blank'>IMDb</a> 
+                    <p class='narration'><strong>Wikipedia Storyline: </strong>${item.wikipedia_storyline}</p>
+                    <p class='narration'><strong>IMDb Storyline: </strong>${item.imdb_storyline}</p>
+                    <p class='narration'><strong>Trivia: </strong>${item.trivia}</p>
+                    <img id='image' src=${item.img} alt='${item.title}' title='${item.title}' />
+                    <p class='narration'><strong>Cast: </strong>${item.cast}</p>
+                    <p class='narration'><strong>Guest Appearances: </strong><br />${item.guest_appearances.join("<br />")}</p>
                 </div>
+                <br />
             `;
         });
     })
